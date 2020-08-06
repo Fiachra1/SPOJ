5595ec276922c53b0684baf812e8b17fc2d8ea0a
Fiachra Merwick <fiachra.merwick@gmail.com>
2020-08-06 11:34:33 +0100

    Update to include recursion

--- Bytelandian_gold_coins.py
+++ Bytelandian_gold_coins.py
@@ -3,13 +3,15 @@ import math
 
 #Need to create a recursive function for this so that 24=27
 def exchange(num):
-    exch=0
-    exch_check=0
-    for i in range(2,5):
-        exch_check=math.floor(num/i)
-        if exch_check
-        exchange(exch)
-    return exch
+    nv=0
+    for i in range(1,4):
+        xi=math.floor(num/(i+1))
+        if xi<12:
+            ni=xi
+        else:
+            ni=exchange(xi)
+        nv+=ni
+    return nv
         
 while True:
     try:
