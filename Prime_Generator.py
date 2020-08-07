9fb846a0c50a50469a963502da2acbdb1e7bf767
Fiachra Merwick <fiachra.merwick@gmail.com>
2020-08-07 01:45:05 +0100

    Function added

--- Prime_Generator.py
+++ Prime_Generator.py
@@ -1,15 +1,11 @@
-t=int(input())
 #define a function and memoise it for larger numbers
 def prime(a,b):
-
-d
-    for i in range (m,n+1):
+    for i in range (a,b+1):
         if i==1:
             continue
         elif i==2:
             print(2)
             continue
-
         val=2
         while val <= i:
             if val==i:
@@ -20,26 +16,11 @@ d
             else:
                 val +=1 
 
+t=int(input())
 for cases in range (t):
     m,n=input().split()
     m= int(m)
     n=int(n)
-    
-    for i in range (m,n+1):
-        if i==1:
-            continue
-        elif i==2:
-            print(2)
-            continue
-
-        val=2
-        while val <= i:
-            if val==i:
-                print(i)
-                break
-            elif i%val==0:
-                break
-            else:
-                val +=1 
+    prime(m,n)
     print ('')
             
