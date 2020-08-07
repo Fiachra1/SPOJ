0bd15228738911c3503ce75ae86405f369bf7a81
Fiachra Merwick <fiachra.merwick@gmail.com>
2020-08-07 19:57:14 +0100

    Used sympy library

--- Prime_Generator.py
+++ Prime_Generator.py
@@ -1,26 +1,31 @@
-#define a function and memoise it for larger numbers
-def prime(a,b):
-    for i in range (a,b+1):
-        if i==1:
-            continue
-        elif i==2:
-            print(2)
-            continue
-        val=2
-        while val <= i:
-            if val==i:
-                print(i)
-                break
-            elif i%val==0:
-                break
-            else:
-                val +=1 
+import sympy
+
+#def prime(a, b): 
+    #if a <= 2: 
+        #print(2)
+    #else:
+        #for i in range(a, b+1): 
+            #val=2
+            #while val <= i:
+                #if val==i:
+                    #print(i)
+                    #break
+                #elif i%val==0:
+                    #break
+                #else:
+                    #val +=1 
 
 t=int(input())
 for cases in range (t):
     m,n=input().split()
     m= int(m)
     n=int(n)
-    prime(m,n)
+    for i in range (m,n+1):
+        if sympy.isprime(i):
+            print(i)
+        else:
+            continue
+    
+    #prime(m,n)
     print ('')
             
